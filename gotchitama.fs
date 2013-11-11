800e622745a082d1de48f95883c5adcc64f5aad0
Gregor Riegler <gregor.riegler@gmail.com>
2013-11-12 00:21:19 +0100

    Handling of wordlist

--- gotchitama.fs
+++ gotchitama.fs
@@ -3,10 +3,10 @@ require mini-oof.fs
 \ create word list
 wordlist constant gotchitama
 
-\ putt our word list on top of the search order
-get-order mywords swap 1+ set-order
+\ put our word list on top of the search order
+get-order gotchitama swap 1+ set-order
 
-\ add word to word list
-\ get-current gotchitama set-current ( wid )
-\ create someword
-\ ( wid ) set-current
+\ Set current word list to gotchitama
+gotchitama set-current
+
+\ Write our words
