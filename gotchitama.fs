ab2e5f4f17256be4ae7183c6220e3748747ce13c
Gregor Riegler <gregor.riegler@gmail.com>
2013-11-13 21:50:17 +0100

    Initial class definition

--- gotchitama.fs
+++ gotchitama.fs
@@ -10,3 +10,22 @@ get-order gotchitama swap 1+ set-order
 gotchitama set-current
 
 \ Write our words
+
+object class
+   \ health points: 0-100
+   cell var health
+   \ maths skills: 0-100
+   cell var maths
+   cell var happiness
+end-class gotchitama
+
+
+\ takes some "genes" parameters like cleverness from stack and creates an according gotchitama
+\ : create-gotchi
+
+\ create a gotchitama with random "genes"
+\ : create-random-gotchi
+   
+
+
+
