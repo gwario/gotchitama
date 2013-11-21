f77c000afd0cda3ec1c46f361af354d53186458e
Mario Gastegger <mario.gastegger@gmail.com>
2013-11-21 15:12:49 +0100

    fix minor bug in "log" and extend name length range from 5-10 to 3-10(for short names like joe, tom, tim, mark, lili, tine, ...)

--- test.fs
+++
@@ -1,8 +0,0 @@
- create v4 104 c, 97 c, 108 c, 108 c, 111 c,
-
-s" bla" pad swap move
-\ pad 2 cells dump
-\ pad .
-\ here .
-
- : <constant ( addr u ) name-too-short? header, reveal docon: cfa, , ;
